573020c51450df735d9d5fdca022a618e0a9f521
ndlong78 <33077621+ndlong78@users.noreply.github.com>
2025-01-03 12:10:33 +0700

    1

--- app/utils/date_utils.py
+++ app/utils/date_utils.py
@@ -1,28 +1,13 @@
-from datetime import datetime
-from .lunar_calendar import solar_to_lunar
-
-def get_lunar_date(solar_date):
-    """Convert solar date to lunar date string"""
-    if isinstance(solar_date, str):
-        solar_date = datetime.strptime(solar_date, '%Y-%m-%d')
-    
-    lunar_day, lunar_month, lunar_year = solar_to_lunar(
-        solar_date.year,
-        solar_date.month,
-        solar_date.day
-    )
-    
-    return f"{lunar_day}/{lunar_month}"
-
+# app/utils/date_utils.py
 def get_vietnamese_month_name(month):
-    """Get Vietnamese month name"""
-    months = [
-        "Tháng Một", "Tháng Hai", "Tháng Ba", "Tháng Tư", "Tháng Năm", "Tháng Sáu",
-        "Tháng Bảy", "Tháng Tám", "Tháng Chín", "Tháng Mười", "Tháng Mười Một", "Tháng Mười Hai"
+    vietnamese_month_names = [
+        "Tháng Một", "Tháng Hai", "Tháng Ba", "Tháng Tư", "Tháng Năm",
+        "Tháng Sáu", "Tháng Bảy", "Tháng Tám", "Tháng Chín", "Tháng Mười",
+        "Tháng Mười Một", "Tháng Mười Hai"
     ]
-    return months[month - 1]
+    return vietnamese_month_names[month - 1]
 
-def get_vietnamese_weekday(date):
-    """Get Vietnamese weekday name"""
-    weekdays = ["Chủ Nhật", "Thứ Hai", "Thứ Ba", "Thứ Tư", "Thứ Năm", "Thứ Sáu", "Thứ Bảy"]
-    return weekdays[date.weekday()]
+def get_lunar_date(date):
+    # Logic to calculate the lunar date from the given date
+    # This is a placeholder function
+    return "Lunar Date Placeholder"
