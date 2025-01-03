16ba5444adb6207f09249232d1f860172efcdb71
ndlong78 <33077621+ndlong78@users.noreply.github.com>
2025-01-03 10:59:49 +0700

    1

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 bp = Blueprint('main', __name__)
 
-from . import routes  # Import routes sau khi tạo blueprint
+from . import routes
---
+++ app/utils/__init__.py
@@ -0,0 +1,8 @@
+from .date_utils import get_lunar_date, get_vietnamese_month_name
+from .lunar_calendar import solar_to_lunar
+
+__all__ = [
+    'get_lunar_date',
+    'get_vietnamese_month_name',
+    'solar_to_lunar'
+]
